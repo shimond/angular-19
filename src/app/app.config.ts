2a8f62b35a02eddcd6c45ea13c6a3e14039ab3bb
Shimon Dahan <shimond@any-techs.co.il>
2025-02-07 14:29:21 +0200

    feat: enhance UpsertPerson component with form validation and dynamic email error handling

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,44 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, Injectable, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient  } from '@angular/common/http';
+import { provideHttpClient } from '@angular/common/http';
 
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { DateAdapter, MAT_DATE_LOCALE, MatDateFormats, NativeDateAdapter, provideNativeDateAdapter } from '@angular/material/core';
+
+
+
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CustomDateAdapter extends NativeDateAdapter {
+
+  override deserialize(value: any): Date | null {
+    const date = super.deserialize(value);
+    if (date) {
+      date.setHours(12, 0, 0, 0); // Set time to 12:00 PM
+    }
+    return date;
+  }
+
+  override parse(value: any): Date | null {
+    if (!value) return null;
+    let date = super.parse(value);
+    if (date) {
+      date.setHours(12, 0, 0, 0); // Set time to 12:00 PM
+    }
+    return date;
+  }
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideNativeDateAdapter(),
+    { provide: DateAdapter, useClass: CustomDateAdapter },
+    { provide: MAT_DATE_LOCALE, useValue: 'he-IL' },
     provideHttpClient(),
-    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes), provideAnimationsAsync()]
 };
--- src/app/components/upsert-person/upsert-person.component.ts
+++ src/app/components/upsert-person/upsert-person.component.ts
@@ -1,46 +1,75 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal, WritableSignal } from '@angular/core';
 import { Person } from '../../models/person.model';
-import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormControl, ReactiveFormsModule, ValidationErrors, Validators } from '@angular/forms';
 import { MatInput } from '@angular/material/input'
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { debounce, debounceTime } from 'rxjs';
 import { MatButtonModule } from '@angular/material/button';
 import { JsonPipe } from '@angular/common';
-
+import { MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 @Component({
   selector: 'app-upsert-person',
-  imports: [ReactiveFormsModule, MatInput, MatFormFieldModule, MatButtonModule, JsonPipe],
+  imports: [ReactiveFormsModule, MatInput, MatFormFieldModule, MatButtonModule, JsonPipe, MatSelectModule, MatDatepickerModule],
   templateUrl: './upsert-person.component.html',
   styleUrl: './upsert-person.component.scss'
 })
 export class UpsertPersonComponent {
-  person: Person = {
+
+  personTypes = signal(['Employee', 'Customer', 'Admin']);
+  emailError = signal<string>('');
+
+  person: WritableSignal<Person> = signal({
     id: 90,
     name: 'Name of person',
     type: 'Employee',
     birthdate: new Date(),
     email: 'nameof@gmail.com'
-  };
+  });
 
-  nameField = new FormControl<string>('', [Validators.required, Validators.minLength(4)]);
+  personForm = inject(FormBuilder).group({
+    id: new FormControl<number>(-1),
+    name: new FormControl<string>('', [Validators.required]),
+    type: new FormControl<'Employee' | 'Customer'>('Employee'),
+    birthdate: new FormControl<Date>(new Date()),
+    email: new FormControl<string>('', [Validators.required, Validators.email, this.startWithA])
+  });
+
   constructor() {
-    // this.nameField.disable
-    //  this.nameField.reset()
-    // this.nameField.errors
-    
-    
-    this.nameField.valueChanges
-    .pipe(debounceTime(250))
-    .subscribe((value) => {
-      console.log(`current name value = ${value}`);
-
+    this.personForm.patchValue(this.person());
+    this.personForm.controls.email.valueChanges.subscribe((value) => {
+      this.emailError.set('');
+      if (this.personForm.controls.email.invalid) {
+        if (this.personForm.controls.email.hasError('required')) {
+          this.emailError.set('Email is required');
+        }
+        if (this.personForm.controls.email.hasError('email')) {
+          this.emailError.set('Email is invalid');
+        }
+        if (this.personForm.controls.email.hasError('startWithA')) {
+          this.emailError.set('Start with not allowed.');
+        }
+      }
     });
   }
-  changeToEnable(){
-    this.nameField.enable();
+
+  startWithA(control: AbstractControl): ValidationErrors | null {
+    if (!control.value) {
+      return null;
+    }
+
+    if (control.value.toString().toLowerCase()[0] == 'a') {
+      return {
+        startWithA: { message: 'value start with A', currentValue: control.value }
+      };
+    }
+    return null;
   }
 
-  changeToDisable(){
-    this.nameField.disable();
+
+  save() {
+    const currentValue = this.personForm.value;
+    this.person.set(currentValue as Person);
   }
+
+
 }
